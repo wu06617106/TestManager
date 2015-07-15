696375fa47b6613a16071a2e849ff09465de4bab
Bruce Tian <wu06617106@hotmail.com.tw>
2015-07-15 17:45:41 +0800

    add login ui

--- TestCaseManager/Controllers/HomeController.cs
+++ TestCaseManager/Controllers/HomeController.cs
@@ -8,7 +8,7 @@ namespace TestCaseManager.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public ActionResult Login()
         {
             ViewBag.Title = "Home Page";
 
