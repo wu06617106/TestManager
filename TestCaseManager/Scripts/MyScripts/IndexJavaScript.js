a569a4f36bc35948e5479aa6a4ba4aa3db525026
Bruce Tian <wu06617106@hotmail.com.tw>
2015-07-21 15:11:46 +0800

    Add Test Cases Tree Add "insert test case by input  title" function to the test cases tree node

--- TestCaseManager/Scripts/MyScripts/IndexJavaScript.js
+++ TestCaseManager/Scripts/MyScripts/IndexJavaScript.js
@@ -14,10 +14,15 @@
         };
 
         //create new tree
-        $scope.createNewTree = function () {
-            if ($scope.tree.length == 0)
-            {
-                $scope.tree.push({"id":1,"title":"node","nodes":[]})
+        $scope.insertNewNode = function () {
+            var length = $scope.tree.length;
+            if (length == 0) {
+                $scope.tree.push({ "id": 1, "title": "node", "nodes": [] });
+            }
+            else {
+                //----------------need to change id's method------------//
+                var lastNode = $scope.tree[length - 1];
+                $scope.tree.push({ "id": lastNode.id + 1, "title": "node", "nodes": [] });
             }
         };
 
@@ -52,8 +57,47 @@
             nodeData.nodes.push({
                 id: nodeData.id * 10 + nodeData.nodes.length,
                 title: nodeData.title + '.' + (nodeData.nodes.length + 1),
-                nodes: []
+                nodes: [],
+                testcases: []
             });
+        };
+        //create new test case for current tree node
+        $scope.newTestCase = function (node) {
+            var nodeData = node.$modelValue;
+            nodeData.testcases.push({
+                id: nodeData.id * 10 + nodeData.testcases.length,
+                title: nodeData.title + '.' + (nodeData.testcases.length + 1),
+                nodes: [],
+                testcases: []
+            });
+        };
+
+        //create new input name test case for current tree node
+        $scope.inputTestCase = {title:""};
+        $scope.newInutNameTestCase = function (node) {
+            var nodeData = node.$modelValue;
+            nodeData.testcases.push({
+                id: nodeData.id * 10 + nodeData.testcases.length,
+                title: $scope.inputTestCase.title,
+                nodes: [],
+                testcases: []
+            });
+        };
+
+        //open test case input
+        
+        $scope.openInputTestCase = function (node) {
+            $scope.selected = node;
+        };
+
+
+        $scope.isNodeSelected = function (node) {
+            return $scope.selected === node
+        };
+
+        //close test case input
+        $scope.closeInputTestCase = function (node) {
+            $scope.selected = "";
         };
 
         //temp tree data
@@ -61,27 +105,28 @@
             "id": 1,
             "title": "tree - item1",
             "nodes": [],
+            "testcases": []
         }, {
             "id": 2,
             "title": "tree - item2",
             "nodes": [],
+            "testcases": []
         }, {
             "id": 3,
             "title": "tree - item3",
             "nodes": [],
+            "testcases": []
         }, {
             "id": 4,
             "title": "tree - item4",
             "nodes": [],
+            "testcases": []
         }];
     });
 
     //Modal Instance controller
-    
     indexApp.controller('ModalInstanceCtrl', function ($scope, $modalInstance, title) {
         $scope.editNodeTitle = title;
-        
-
         $scope.ok = function () {
             $modalInstance.close($scope.editNodeTitle);
         };
